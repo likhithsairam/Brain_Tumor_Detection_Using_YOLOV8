e4d1d02e0198a55bdbd9f8c5757e30bd56e6c814
LIKHITH SAI RAM <likhithsairam26@gmail.com>
2025-09-03 20:23:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO("yolov8m.pt")
-    results = model.train(data="C:\\Users\\likhi\\PycharmProjects\\PythonProject\\Brain-Tumor-Detection-1\\data.yaml",
+    results = model.train(data="Brain-Tumor-Detection-1/data.yaml",
                           epochs=50,
                           imgsz=640,
                           batch=8,
